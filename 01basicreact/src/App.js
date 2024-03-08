8b59662a73352b740e106149abea44c2d6bce14d
Ansh439 <gupta.ansh3150@gmail.com>
2024-03-08 08:19:22 +0530

    initialise react using vite and create-react-app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello World | Ansh Gupta</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Hello World with VITE | Ansh</h1>
+  )
+}
+
+export default App
