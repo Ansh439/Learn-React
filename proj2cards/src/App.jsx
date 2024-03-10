d7a0a4641ac9d7f85e724e06ee7038ceb6e63356
Ansh439 <gupta.ansh3150@gmail.com>
2024-03-10 10:26:51 +0530

    Tailwind-Components-ReactProps

---
+++ proj2cards/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './Components/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Card desc = "Macbook" btn = "View More"/>
+      <Card desc = "Dell" btn = "Click Here"/>
+      <Card desc = "Lava" btn = "Add to Cart"/>
+    </>
+  )
+}
+
+export default App
