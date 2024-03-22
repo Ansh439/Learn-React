cde8f993d3738d79e5b88acd26acbea575461bef
Ansh439 <gupta.ansh3150@gmail.com>
2024-03-22 22:05:15 +0530

    env variables set-up for MegaBlog

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1>A Blog App in Appwrite</h1>
+    </>
+  )
+}
+
+export default App
