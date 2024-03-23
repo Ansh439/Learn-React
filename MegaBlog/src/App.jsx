2c32ae550c01d7083d540dde298dd7d025a034c6
Ansh439 <gupta.ansh3150@gmail.com>
2024-03-23 08:56:40 +0530

    Redux Store configured

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,12 +1,42 @@
+import { useState, useEffect } from 'react';
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
 import './App.css'
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A Blog App in Appwrite</h1>
-    </>
-  )
+  
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}));
+      } else{
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
+
+
+  return !loading ? (
+    <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
