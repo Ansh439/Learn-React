b0c486c6de491d570ddee8904c78e741fbfa4b96
Ansh439 <gupta.ansh3150@gmail.com>
2024-03-26 00:47:39 +0530

    organised

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
 
   return !loading ? (
-    <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+    <div className='flex min-h-screen flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
         <Header />
         <main>
--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,4 +1,4 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 
-export{Header, Footer}
+export {Header, Footer}
